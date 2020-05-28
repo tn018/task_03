a977b03d4da41d4f7d10e3c6abab5efd56c77e06
Никита Т <65958048+tn018@users.noreply.github.com>
2020-05-28 14:49:49 +0300

    Update task_03_4O-506C_Tyrkalov_07.py

--- task_03_4O-506C_Tyrkalov_07.py
+++ task_03_4O-506C_Tyrkalov_07.py
@@ -10,16 +10,16 @@ if __name__ == '__main__':
 
     # Размер области моделирования
     x = 3
-
+    
     # Число Куранта
     Sc = 1.0
 
     # Скорость света
     c = 3e8
-
+    
     # Шаг по времени
     dt = Sc * dx / c
-
+    
     # Время расчета в отсчетах
     maxTime = 450
 
@@ -30,15 +30,47 @@ if __name__ == '__main__':
     sourcePos = 50
 
     # Датчики для регистрации поля
-    probesPos = [125]
+    probesPos = [160]
     probes = [tools.Probe(pos, maxTime) for pos in probesPos]
 
+    # Параметры среды
+    # Диэлектрическая проницаемость
+    eps = numpy.ones(maxSize)
+
+    # Магнитная проницаемость
+    mu = numpy.ones(maxSize - 1)
+
     Ez = numpy.zeros(maxSize)
-    Hy = numpy.zeros(maxSize)
+    Hy = numpy.zeros(maxSize - 1)
 
-    for probe in probes:
-        probe.addData(Ez, Hy)
+    # Коэффициенты для расчета ABC второй степени
+    # Sc' для левой границы
+    Sc1Left = Sc / numpy.sqrt(mu[0] * eps[0])
 
+    k1Left = -1 / (1 / Sc1Left + 2 + Sc1Left)
+    k2Left = 1 / Sc1Left - 2 + Sc1Left
+    k3Left = 2 * (Sc1Left - 1 / Sc1Left)
+    k4Left = 4 * (1 / Sc1Left + Sc1Left)
+
+    # Sc' для правой границы
+    Sc1Right = Sc / numpy.sqrt(mu[-1] * eps[-1])
+
+    k1Right = -1 / (1 / Sc1Right + 2 + Sc1Right)
+    k2Right = 1 / Sc1Right - 2 + Sc1Right
+    k3Right = 2 * (Sc1Right - 1 / Sc1Right)
+    k4Right = 4 * (1 / Sc1Right + Sc1Right)
+
+    # Ez[0: 2] в предыдущий момент времени (q)
+    oldEzLeft1 = numpy.zeros(3)
+
+    # Ez[0: 2] в пред-предыдущий момент времени (q - 1)
+    oldEzLeft2 = numpy.zeros(3)
+
+    # Ez[-3: -1] в предыдущий момент времени (q)
+    oldEzRight1 = numpy.zeros(3)
+
+    # Ez[-3: -1] в пред-предыдущий момент времени (q - 1)
+    oldEzRight2 = numpy.zeros(3)
     # Параметры отображения поля E
     display_field = Ez
     display_ylabel = 'Ez, В/м'
@@ -52,9 +84,9 @@ if __name__ == '__main__':
                                         display_ylabel, dx)
 
     display.activate()
-    display.drawSources([sourcePos])
     display.drawProbes(probesPos)
-    
+    display.drawSources([sourcePos])
+
     # Параметры гауссова импульса
     A0 = 100  # ослабление в 0 момент времени по отношение к максимуму
     Am = 100  # ослабление на частоте Fm
@@ -64,42 +96,49 @@ if __name__ == '__main__':
     dg = wg * numpy.sqrt(numpy.log(A0))
     NDg = dg / dt
 
-    for t in range(1, maxTime):
-        # Граничные условия для поля H
-        Hy[-1] = Hy[-2]
-
+    
+    for t in range(maxTime):
         # Расчет компоненты поля H
-        Ez_shift = Ez[1:]
-        Hy[:-1] = Hy[:-1] + (Ez_shift - Ez[:-1]) * Sc / W0
+        Hy = Hy + (Ez[1:] - Ez[:-1]) * Sc / (W0 * mu)
 
         # Источник возбуждения с использованием метода
         # Total Field / Scattered Field
-        Hy[sourcePos - 1] -= (Sc / W0) * \
-            numpy.exp(-((t - NDg - sourcePos) / NWg) ** 2)
-        # Hy[sourcePos - 1] -= (Sc / W0) * numpy.exp(-((t - NDg)/NWg) ** 2 )
-
-        # Граничные условия для поля E
-        Ez[0] = Ez[1]
+        Hy[sourcePos - 1] -= (Sc / (W0 * mu[sourcePos - 1]) *
+                              numpy.exp(-(t - 30.0) ** 2 / 100.0))
 
         # Расчет компоненты поля E
-        Hy_shift = Hy[:-1]
-        Ez[1:] = Ez[1:] + (Hy[1:] - Hy_shift) * Sc * W0
+        Hy_shift = Hy[: -1]
+        Ez[1:-1] = Ez[1: -1] + (Hy[1:] - Hy_shift) * Sc * W0 / eps[1: -1]
 
         # Источник возбуждения с использованием метода
         # Total Field / Scattered Field
-        Ez[sourcePos] += Sc * \
-            numpy.exp(-(((t + 0.5) - (sourcePos - 0.5) - NDg) / NWg) ** 2)
-        # Ez[sourcePos] += Sc * numpy.exp(-(((t + 0.5) - (-0.5) - NDg)/NWg) ** 2 )
+        Ez[sourcePos] += (Sc / (numpy.sqrt(eps[sourcePos] * mu[sourcePos])) *
+                          numpy.exp(-((t + 0.5) - (-0.5 * numpy.sqrt(eps[sourcePos] * mu[sourcePos]) / Sc) - 30.0) ** 2 / 100.0))
+
+        # Граничные условия ABC второй степени (слева)
+        Ez[0] = (k1Left * (k2Left * (Ez[2] + oldEzLeft2[0]) +
+                           k3Left * (oldEzLeft1[0] + oldEzLeft1[2] - Ez[1] - oldEzLeft2[1]) -
+                           k4Left * oldEzLeft1[1]) - oldEzLeft2[2])
+
+        oldEzLeft2[:] = oldEzLeft1[:]
+        oldEzLeft1[:] = Ez[0: 3]
+
+        # Граничные условия ABC второй степени (справа)
+        Ez[-1] = (k1Right * (k2Right * (Ez[-3] + oldEzRight2[-1]) +
+                             k3Right * (oldEzRight1[-1] + oldEzRight1[-3] - Ez[-2] - oldEzRight2[-2]) -
+                             k4Right * oldEzRight1[-2]) - oldEzRight2[-3])
+
+        oldEzRight2[:] = oldEzRight1[:]
+        oldEzRight1[:] = Ez[-3:]
 
         # Регистрация поля в датчиках
         for probe in probes:
             probe.addData(Ez, Hy)
 
-        if t % 4 == 0:
+        if t % 5 == 0:
             display.updateData(display_field, t)
 
     display.stop()
-
     # Отображение сигнала, сохраненного в датчиках
     tools.showProbeSignals(probes, -1.1, 1.1, dt)
 
